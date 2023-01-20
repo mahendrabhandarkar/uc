301fd94bc0e2a25a52acc66786dbd9af51de98da
Mahendra Bhandarkar <mahendra.bhandarkar@expleogroup.com>
2023-01-20 21:27:38 +0530

    MultiServices

--- backend/demo/src/main/java/com/uc/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/uc/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
